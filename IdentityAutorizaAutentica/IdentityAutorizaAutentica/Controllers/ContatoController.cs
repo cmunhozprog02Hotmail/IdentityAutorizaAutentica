094a153221d59d00492e5ab77ded1b86fe5361aa
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2018-06-07 15:53:46 -0300

    Identity parte 1 Authorize allowAnonymous

---
+++ IdentityAutorizaAutentica/IdentityAutorizaAutentica/Controllers/ContatoController.cs
@@ -0,0 +1,35 @@
+﻿using IdentityAutorizaAutentica.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace IdentityAutorizaAutentica.Controllers
+{
+    [Authorize]
+    public class ContatoController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ContatoController(ApplicationDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        public IActionResult AcessoAutoruzado()
+        {
+            return Content("ACESSO AUTORIZADO");
+        }
+
+        [AllowAnonymous]
+        public IActionResult AcessoAnonimo()
+        {
+            return Content("ACESSO ANONIMO");
+        }
+    }
+}

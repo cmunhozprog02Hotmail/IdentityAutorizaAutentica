6cd3b3a5ad5a3810c3bbb5079929e8b4180b4340
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-06-07 12:14:36 -0300

    Incluir Tabela Contatos e add-migrations

--- IdentityAutorizaAutentica/IdentityAutorizaAutentica/Data/Migrations/20180607151337_IncluirTabelaContatos.Designer.cs
+++ IdentityAutorizaAutentica/IdentityAutorizaAutentica/Data/Migrations/20180607151337_IncluirTabelaContatos.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace IdentityAutorizaAutentica.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180607151337_IncluirTabelaContatos")]
+    partial class IncluirTabelaContatos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
